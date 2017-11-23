51daa60893396a9c125c84a651e5a6884e3eaa00
Naomi Aro <naomiaro@gmail.com>
2017-11-23 00:59:44 +0000

    Fix bug with automatic scroll

--- src/render/ScrollHook.js
+++ src/render/ScrollHook.js
@@ -1,4 +1,4 @@
-import { secondsToPixels } from '../utils/conversions';
+import { secondsToPixels, pixelsToSeconds } from '../utils/conversions';
 
 /*
 * virtual-dom hook for scrolling the track container.
@@ -15,20 +15,28 @@ export default class {
 
       if (playlist.isAutomaticScroll && playlist.isPlaying()) {
         const rect = node.getBoundingClientRect();
+        const windowTime = pixelsToSeconds(
+          rect.width,
+          playlist.samplesPerPixel,
+          playlist.sampleRate,
+        );
+
+        const MAX_SCROLL_LEFT = playlist.duration - windowTime;
+
         const cursorNode = node.querySelector('.cursor');
         if (cursorNode) {
           const cursorRect = cursorNode.getBoundingClientRect();
 
           if (cursorRect.right > rect.right || cursorRect.right < 0) {
-            playlist.scrollLeft = playlist.playbackSeconds;
+            playlist.scrollLeft = Math.min(playlist.playbackSeconds, MAX_SCROLL_LEFT);
           }
         }
       }
 
       const left = secondsToPixels(
-          playlist.scrollLeft,
-          playlist.samplesPerPixel,
-          playlist.sampleRate,
+        playlist.scrollLeft,
+        playlist.samplesPerPixel,
+        playlist.sampleRate,
       );
 
       el.scrollLeft = left;
