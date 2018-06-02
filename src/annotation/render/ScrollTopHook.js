dbe05a2beac1b163aecd29dad6810be0a089d460
Naomi Aro <naomiaro@gmail.com>
2018-06-02 23:31:53 +0300

    Fix grabbing first item.

--- src/annotation/render/ScrollTopHook.js
+++ src/annotation/render/ScrollTopHook.js
@@ -3,14 +3,18 @@
 */
 const Hook = function ScrollTopHook() {};
 Hook.prototype.hook = function hook(node) {
-  const el = node.querySelector('.current');
-  if (el) {
-    const box = node.getBoundingClientRect();
-    const row = el.getBoundingClientRect();
-    const diff = row.top - box.top;
-    const list = node;
-    list.scrollTop += diff;
-  }
+  setTimeout(() => {
+    const el = document.querySelector('.current');
+    console.log(node.children[0]);
+    console.log(el);
+    if (el) {
+      const box = node.getBoundingClientRect();
+      const row = el.getBoundingClientRect();
+      const diff = row.top - box.top;
+      const list = node;
+      list.scrollTop += diff;
+    }
+  }, 0);
 };
 
 export default Hook;
